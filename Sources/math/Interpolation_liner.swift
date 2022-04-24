d2a1e1b879b3db13b675b0380dcc0e00580f21d5
Vitaliy Teleusov <Vitaliy82tvn@gmail.com>
2022-04-25 02:30:18 +0300

    Change access level for function

--- Sources/math/Interpolation_liner.swift
+++ Sources/math/Interpolation_liner.swift
@@ -18,11 +18,11 @@ public protocol Interpolation_Line {
 
 public extension Interpolation_Line {
     
-    func lineFunctionValue(inArgumentValue x: CGFloat,
-                           firstFunctionKnownValue y1: CGFloat,
-                           secondFunctionKnownValue y2: CGFloat,
-                           firstFunctionKnownArgument x1: CGFloat,
-                           secondFunctionKnownArgument x2: CGFloat) -> CGFloat {
+    public func lineFunctionValue(inArgumentValue x: CGFloat,
+                                  firstFunctionKnownValue y1: CGFloat,
+                                  secondFunctionKnownValue y2: CGFloat,
+                                  firstFunctionKnownArgument x1: CGFloat,
+                                  secondFunctionKnownArgument x2: CGFloat) -> CGFloat {
         
         return (x - y2) * ((x2 - x1) / (y2 - y1)) + x2
     }
